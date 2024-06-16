e9e094f0c633576157274c453e438778325bf06c
BI4VMR <bi4vmr@outlook.com>
2024-06-17 00:25:33 +0800

    EF9F70C9-393F-4440-8E9B-AE622286A2EA

--- M03_Advance/C03_Concurrent/src/main/kotlin/net/bi4vmr/study/base/TestCoroutine.kt
+++ M03_Advance/C03_Concurrent/src/main/kotlin/net/bi4vmr/study/base/TestCoroutine.kt
@@ -22,43 +22,100 @@ fun example01() {
     // 阻塞主线程，等待协程执行完毕。
     runBlocking {
         /*
-         * 使用Default调度器，启动协程并提交任务。
+         * 使用Default调度器，提交任务并启动协程。
          *
          * 此处开启协程时需要调用"join()"方法，否则主线程结束后整个进程将会终止，子线程（协程任务）不会继续执行。
          */
         CoroutineScope(Dispatchers.Default).launch {
-            println("Coroutine exec start. Time:[${getTime()}]")
+            println("Task start. Name:[${getThread()}] Time:[${getTime()}]")
             // 延时2秒，模拟耗时操作。
             delay(2000)
-            println("Coroutine exec end. Time:[${getTime()}]")
+            println("Task end. Name:[${getThread()}] Time:[${getTime()}]")
         }.join()
     }
 }
 
 /*
- * 示例：顺序执行。
+ * 示例：挂起函数。
  */
 fun example02() {
+    val scope = CoroutineScope(Dispatchers.Default)
+    // 开启第一个任务
+    scope.launch {
+        function1()
+    }
+    // 开启第二个任务
+    scope.launch {
+        function2()
+    }
+
+    // 阻塞主线程5秒，避免协程提前终止。
+    Thread.sleep(5000L)
+}
+
+/*
+ * 示例：顺序执行多个任务。
+ *
+ * 协程体中的任务默认按照顺序执行，前一个任务执行完毕后，后一个任务才会开始执行。
+ */
+fun example022() {
     runBlocking {
-        CoroutineScope(Dispatchers.Default).launch {
-            // 先执行第一个任务
-            function1()
-            // 第一个任务执行完毕后，再执行第二个任务。
-            function2()
-        }.join()
+        // 先执行第一个任务
+        function1()
+        // 第一个任务执行完毕后，再执行第二个任务。
+        function2()
+        // CoroutineScope(Dispatchers.Default).launch {
+        //     // 先执行第一个任务
+        //     function1()
+        //     // 第一个任务执行完毕后，再执行第二个任务。
+        //     function2()
+        // }.join()
     }
 }
 
-suspend fun function1() {
-    println("Suspend function1 exec start. Time:[${getTime()}]")
-    delay(2000)
-    println("Suspend function1 exec end. Time:[${getTime()}]")
+/*
+ * 示例：并发执行多个任务。
+ */
+fun example03() {
+    runBlocking {
+        println("async start")
+        val a = async { function1() }
+        delay(5000)
+        a.await()
+        println("async end")
+    }
 }
 
-suspend fun function2() {
-    println("Suspend function2 exec start. Time:[${getTime()}]")
+/*
+ * 示例：等待多个任务完成再继续执行。
+ */
+fun example04() {
+    runBlocking {
+
+        val job1: Deferred<Int> = async { function1() }
+        val job2: Deferred<Int> = async { function2() }
+
+        val r1 = job1.await()
+        val r2 = job2.await()
+
+        println("r=${r1 + r2}")
+    }
+}
+
+// 测试方法一
+suspend fun function1(): Int {
+    println("Task1 start. Name:[${getThread()}] Time:[${getTime()}]")
+    delay(2000)
+    println("Task1 end. Name:[${getThread()}] Time:[${getTime()}]")
+    return 7
+}
+
+// 测试方法二
+suspend fun function2(): Int {
+    println("Task2 start. Name:[${getThread()}] Time:[${getTime()}]")
     delay(3000)
-    println("Suspend function2 exec end. Time:[${getTime()}]")
+    println("Task2 end. Name:[${getThread()}] Time:[${getTime()}]")
+    return 3
 }
 
 /**
@@ -69,4 +126,13 @@ suspend fun function2() {
 private fun getTime(): String {
     val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS")
     return LocalTime.now().format(dateFormatter)
+}
+
+/**
+ * 获取当前线程名称。
+ *
+ * @return 线程名称。
+ */
+private fun getThread(): String {
+    return Thread.currentThread().name
 }
