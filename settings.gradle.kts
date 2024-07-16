3b5a9c0a1f9d3a1e30bd67c84f0605e99ab81771
BI4VMR <bi4vmr@outlook.com>
2024-07-16 18:08:55 +0800

    98F4AF2F-D715-41E1-A568-435C38E34DBE

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -103,4 +103,4 @@ include(":M02_Syntax:C01_Struct")
 
 /* ----- 高级特性 ----- */
 include(":M03_Advance:C01_OOP")
-include(":M03_Advance:C03_Parallelism:S02_Coroutine")
+include(":M03_Advance:C03_Concurrent:S02_Coroutine")
