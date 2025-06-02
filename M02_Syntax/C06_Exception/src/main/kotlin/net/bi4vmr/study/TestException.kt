883174564d2f97333dea682082c73bc6a12feb76
BI4VMR <bi4vmr@outlook.com>
2025-06-02 12:47:22 +0800

    42FE9883-1E33-4827-88AB-80E0C2CEE769

--- M02_Syntax/C06_Exception/src/main/kotlin/net/bi4vmr/study/TestException.kt
+++ M02_Syntax/C06_Exception/src/main/kotlin/net/bi4vmr/study/TestException.kt
@@ -1,6 +1,7 @@
 package net.bi4vmr.study
 
 import net.bi4vmr.study.exception.CustomException
+import java.util.*
 
 /**
  * 测试代码 - 异常。
@@ -9,27 +10,107 @@ import net.bi4vmr.study.exception.CustomException
  * @since 1.0.0
  */
 fun main() {
+    example04()
+}
+
+/**
+ * 示例一：异常捕获语句的基本应用。
+ *
+ * 在本示例中，我们将两个整数相除的语句放置在 `try-catch-finally` 语句中，使用不同的值多次运行程序，对比程序的行为。
+ */
+fun example01() {
     try {
-        convertException()
+        val i = 10 / 0
+        println("i=$i")
+    } catch (e: java.lang.Exception) {
+        println("发生异常！")
+    } finally {
+        println("善后工作完成！")
+    }
+    println("整个程序已结束！")
+}
+
+/**
+ * 示例二：在控制台上显示异常详情。
+ *
+ * 在本示例中，我们故意制造一个异常，捕获异常后使用 `printStackTrace()` 方法将详情输出到控制台上。
+ */
+fun example02() {
+    try {
+        val i = 10 / 0
     } catch (e: Exception) {
         e.printStackTrace()
     }
 }
 
 /**
- * 示例：变量的基本应用。
+ * 示例三：捕获多种类型的异常。
  *
- * 在本示例中，我们定义一些变量，并访问它们。
+ * 在本示例中，我们使用多个 `catch` 块，分别捕获不同类型的异常。
  */
-fun example02() {
-    // 声明变量“姓名”
-    val name: String = "张三"
-    // 声明变量“年龄”
-    val age: Int = 20
+fun example03() {
+    try {
+        val i = 10 / 0
+    } catch (ae: ArithmeticException) {
+        println("除数不能为0！")
+    } catch (e: Exception) {
+        println("其它错误。")
+    }
+}
 
-    // 访问变量：将变量的值输出到控制台
-    println("姓名：$name")
-    println("年龄：$age")
+/**
+ * 示例四：异常与 `return` 语句。
+ *
+ * 在本示例中，我们故意制造一个异常，然后在 `try-catch-finally` 语句中添加 `return` 语句，测试跳转规则。
+ */
+fun example04() {
+    val result = returnInTryCatch()
+    println(result)
+}
+
+fun returnInTryCatch(): String {
+    try {
+        10 / 0
+        return "try块中的return生效了。"
+    } catch (e: Exception) {
+        // 出现异常情况的返回值
+        return "catch块中的return生效了。"
+    } finally {
+        return "finally块中的return生效了。"
+    }
+}
+
+/**
+ * 示例五："try-with-resources"语法的基本应用。
+ *
+ * 在本示例中，我们从控制台读取一行文本，并将其写入文件。
+ */
+fun example05() {
+    val scanner = Scanner(System.`in`)
+    scanner.use {  }.
+    // val writer = BufferedWriter(scanner)
+}
+
+/**
+ * 示例六：主动抛出异常。
+ *
+ * 在本示例中，我们编写一个方法用于计算整数角度的正切值，当输入值为90度的倍数时，向调用者抛出算术异常。
+ */
+fun example06() {
+    try {
+        println("tan(45) = ${tan(45)}")
+        println("tan(90) = ${tan(90)}")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+fun tan(deg: Int): Double {
+    if (deg % 90 == 0) {
+        throw ArithmeticException()
+    } else {
+        return kotlin.math.tan(Math.toRadians(deg.toDouble()))
+    }
 }
 
 /**
