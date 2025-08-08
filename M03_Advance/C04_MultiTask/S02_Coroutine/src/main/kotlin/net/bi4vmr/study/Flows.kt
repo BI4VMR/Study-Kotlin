a9f7e1b76d5ca5e2a1bf9052b7fc85a725f71783
BI4VMR <bi4vmr@outlook.com>
2025-08-08 18:03:45 +0800

    AA81506D-0E32-40FB-90A1-AD775903D21A

--- M03_Advance/C04_MultiTask/S02_Coroutine/src/main/kotlin/net/bi4vmr/study/Flows.kt
+++ M03_Advance/C04_MultiTask/S02_Coroutine/src/main/kotlin/net/bi4vmr/study/Flows.kt
@@ -5,18 +5,25 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
-val flow = MutableStateFlow(1)
+val flow = MutableStateFlow(111)
 
 fun main() {
     CoroutineScope(Dispatchers.Default).launch {
         delay(1000L)
         flow.value = 111
+        delay(1000L)
+        flow.emit(111)
     }
 
-    CoroutineScope(Dispatchers.Default).launch {
-        flow.collect {
-            println("$it")
+        CoroutineScope(Dispatchers.Default).launch {
+            flow.collect {
+                println("$it")
+            }
         }
+
+    runBlocking {
+        delay(5000L)
     }
 }
