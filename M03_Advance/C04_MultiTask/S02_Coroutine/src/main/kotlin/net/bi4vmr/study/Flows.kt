1bf94abde65304dff7a79a341c0e9f4113164671
BI4VMR <bi4vmr@outlook.com>
2025-09-27 00:02:57 +0800

    01BFCDD1-3560-4A35-AD12-93AEF707D216

--- M03_Advance/C04_MultiTask/S02_Coroutine/src/main/kotlin/net/bi4vmr/study/Flows.kt
+++ M03_Advance/C04_MultiTask/S02_Coroutine/src/main/kotlin/net/bi4vmr/study/Flows.kt
@@ -1,29 +1,55 @@
 package net.bi4vmr.study
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 val flow = MutableStateFlow(111)
 
+var count = 0
+val mutex: Mutex = Mutex()
+
 fun main() {
-    CoroutineScope(Dispatchers.Default).launch {
-        delay(1000L)
-        flow.value = 111
-        delay(1000L)
-        flow.emit(111)
+    // CoroutineScope(Dispatchers.Default).launch {
+    //     delay(1000L)
+    //     flow.value = 111
+    //     delay(1000L)
+    //     flow.emit(111)
+    // }
+    //
+    // CoroutineScope(Dispatchers.Default).launch {
+    //     flow.collect {
+    //         println("$it")
+    //     }
+    // }
+    //
+    // runBlocking {
+    //     delay(5000L)
+    // }
+
+    suspend fun change(caller: String) {
+        delay(10L)
+        mutex.withLock {
+            count++
+            println("$caller -> $count  T:[${Thread.currentThread().name}]")
+        }
+
     }
 
-        CoroutineScope(Dispatchers.Default).launch {
-            flow.collect {
-                println("$it")
+    runBlocking {
+        CoroutineScope(Dispatchers.IO).launch {
+            repeat(10) {
+                change("A")
             }
         }
 
-    runBlocking {
-        delay(5000L)
+        CoroutineScope(Dispatchers.IO).launch {
+            repeat(10) {
+                change("B")
+            }
+        }
+
+        delay(3000L)
     }
 }
