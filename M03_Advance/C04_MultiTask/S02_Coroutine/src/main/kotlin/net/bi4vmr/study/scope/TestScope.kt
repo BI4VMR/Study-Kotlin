4d3b90ad273c7c3276ecc8c0f0d6f6985e687481
BI4VMR <bi4vmr@outlook.com>
2026-01-06 17:55:01 +0800

    7F6CF4A8-0D70-4051-AF02-628F07CF4DFE

--- M03_Advance/C04_MultiTask/S02_Coroutine/src/main/kotlin/net/bi4vmr/study/scope/TestScope.kt
+++ M03_Advance/C04_MultiTask/S02_Coroutine/src/main/kotlin/net/bi4vmr/study/scope/TestScope.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 /**
@@ -26,45 +27,62 @@ fun example01() {
 }
 
 
-class UserManager {
+/*
+ * 示例：编码风格。
+ */
 
-    private var currentUID: String = "0"
+// 不推荐的用法
+class UserManager {
 
     fun updateCurrentUID() {
         CoroutineScope(Dispatchers.IO).launch {
-            // 模拟耗时操作完成后更新结果
-            currentUID = "User1"
+            // 模拟耗时操作...
+            delay(1000L)
+        }
+    }
+
+    fun updateCurrentName() {
+        CoroutineScope(Dispatchers.IO).launch {
+            // 模拟耗时操作...
+            delay(1000L)
         }
     }
 }
 
+// 推荐用法一：由调用者提供协程环境。
+class UserManagerV1 {
 
-class UserManager2 {
+    suspend fun updateCurrentUID(): String = "1"
 
-    // 模拟耗时操作完成后返回结果
-    suspend fun updateCurrentUID(): String = "User1"
+    suspend fun updateCurrentName(): String = "User1"
 }
 
-
-class UserManager3(
+// 推荐用法二：调用者无法使用协程，由当前类维护协程环境。
+class UserManagerV2(
     // 使用外部传入的调度器，或提供默认的调度器。
     dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
 
-    // Manager内的业务使用统一作用域
+    // 所有业务使用统一的作用域
     private val scope = CoroutineScope(dispatcher)
-
-    private var currentUID: String = "0"
 
     fun updateCurrentUID() {
         // 方法内部不要单独创建作用域
         scope.launch {
-            // 模拟耗时操作完成后更新结果
-            currentUID = "User1"
+            // 模拟耗时操作...
+            delay(1000L)
         }
     }
 
-    // 提供组件销毁方法，取消作用域的所有协程。
+    fun updateCurrentName() {
+        // 方法内部不要单独创建作用域
+        scope.launch {
+            // 模拟耗时操作...
+            delay(1000L)
+        }
+    }
+
+    // 提供组件销毁方法，取消当前作用域的所有协程。
     fun destroy() {
         scope.cancel()
     }
