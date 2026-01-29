b37aa6394d59305e661115ef2cd42cfd4b643e63
BI4VMR <bi4vmr@outlook.com>
2026-01-29 18:01:56 +0800

    3561B80F-AFEA-4522-A401-78B936B5FD8C

---
+++ M03_Advance/C04_MultiTask/S02_Coroutine/src/main/kotlin/net/bi4vmr/study/sync/TestSync.kt
@@ -0,0 +1,21 @@
+package net.bi4vmr.study.sync
+
+/**
+ * 示例代码：数据同步。
+ *
+ * @author bi4vmr@outlook.com
+ * @since 1.0.0
+ */
+fun main() {
+    example01()
+}
+
+
+/**
+ * 示例一：创建协程作用域。
+ *
+ * 在本示例中，我们创建一些协程作用域，并指定上下文。
+ */
+fun example01() {
+
+}
