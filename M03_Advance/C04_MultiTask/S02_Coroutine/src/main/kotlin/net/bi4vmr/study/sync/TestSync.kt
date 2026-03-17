443a6db4b3f1108981f72860dd70ae8128803d81
BI4VMR <bi4vmr@outlook.com>
2026-03-17 17:54:47 +0800

    E816772E-5634-4999-956A-B89D218BBCB7

--- M03_Advance/C04_MultiTask/S02_Coroutine/src/main/kotlin/net/bi4vmr/study/sync/TestSync.kt
+++ M03_Advance/C04_MultiTask/S02_Coroutine/src/main/kotlin/net/bi4vmr/study/sync/TestSync.kt
@@ -1,5 +1,10 @@
 package net.bi4vmr.study.sync
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+
 /**
  * 示例代码：数据同步。
  *
@@ -16,6 +21,15 @@ fun main() {
  *
  * 在本示例中，我们创建一些协程作用域，并指定上下文。
  */
+var i = 0
 fun example01() {
-
+    // val m: Mutex = Mutex()
+    repeat(100) {
+        CoroutineScope(Dispatchers.IO).launch {
+            // m.withLock {
+            i++
+            // }
+        }
+    }
+    println("i:$i")
 }
