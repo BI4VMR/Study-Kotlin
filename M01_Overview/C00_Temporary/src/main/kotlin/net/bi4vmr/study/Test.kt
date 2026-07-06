140a2c2453b07e58bb38f257b2588fb5fa4af59f
BI4VMR <bi4vmr@outlook.com>
2026-07-06 09:17:26 +0800

    C591400C-1624-406F-8F1D-39CE8F9C55E6

--- M01_Overview/C00_Temporary/src/main/kotlin/net/bi4vmr/study/Test.kt
+++ M01_Overview/C00_Temporary/src/main/kotlin/net/bi4vmr/study/Test.kt
@@ -1,9 +1,6 @@
 package net.bi4vmr.study
 
-import net.bi4vmr.tool.java.external.adb.ADBController
-import net.bi4vmr.tool.kotlin.external.scrcpy.ScreenCastEventListener
-import net.bi4vmr.tool.kotlin.external.scrcpy.ScreenCastManager
-import net.bi4vmr.tool.kotlin.external.scrcpy.ui.ScreenCastWindow
+import java.awt.Toolkit
 
 /**
  * TODO 添加描述。
@@ -12,48 +9,6 @@ import net.bi4vmr.tool.kotlin.external.scrcpy.ui.ScreenCastWindow
  * @since 1.0.0
  */
 fun main() {
-    println("ADB init")
-    ADBController.init()
-    println("ADB init done")
-    ADBController.getDevices().firstOrNull()
-        ?.let {
-            println("ScrcpyUtil $it")
-
-            ScreenCastManager.setServerFilePath("/home/bi4vmr/Work/scrcpy-server")
-            ScreenCastManager.setServerVersion("4.0")
-
-            // val i = object : ScreenCastEventListener {
-            //     override fun onSizeChange(width: Int, height: Int) {
-            //         println("onSizeChange: $width x $height")
-            //     }
-            //
-            //     override fun onNewFrame(
-            //         yData: ByteArray,
-            //         yStride: Int,
-            //         uData: ByteArray,
-            //         uStride: Int,
-            //         vData: ByteArray,
-            //         vStride: Int
-            //     ) {
-            //         println("onNewFrame: yStride: $yStride uStride: $uStride vStride: $vStride")
-            //     }
-            //
-            //     override fun onError(error: Exception) {
-            //         println("onerror: $error : ${error.message}")
-            //         error.printStackTrace()
-            //     }
-            //
-            //     override fun onTitleResolve(title: String) {
-            //         println("onTitleResolve: $title")
-            //     }
-            // }
-            // ScreenCastManager.start(it, i)
-
-            val window = ScreenCastWindow("My Device")
-            window.connect(it)
-        }
-
-    Thread.sleep(99999L)
-    println("full end")
-    // ADBController.terminate()
+    val i = Toolkit.getDefaultToolkit().screenResolution
+    println(i)
 }
